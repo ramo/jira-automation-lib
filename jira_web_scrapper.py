ab8e8d78f6c5a17bb133fd6fdcf987b2c54f71a4
ramo <ramo.phoenix7@gmail.com>
2018-12-27 19:32:14 +0530

    Initial commit jira automation library

---
+++ jira_web_scrapper.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import sys
+
+
+def get_jira_url():
+    return os.environ['JIRA_URL']
+
+
+def get_page_soup(task_id):
+    url = get_jira_url() + "/browse/" + task_id
+    page = get(url)
+    return BeautifulSoup(page.text, 'html.parser')
+
+
+def get_task_description(task_id):
+    soup = get_page_soup(task_id)
+    return soup.find('h1', {'id': 'summary-val'}).text
+
+
+def get(url, **kwargs):
+    cookies = kwargs['cookies'] if 'cookies' in kwargs else {}
+    cookies['cloud.session.token'] = os.environ['JIRA_TOKEN']
+    return requests.get(url, kwargs, cookies=cookies)
+
+
+def main():
+    print(get_task_description(sys.argv[1]))
+
+
+if __name__ == '__main__':
+    main()
